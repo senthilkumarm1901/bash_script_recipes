7e01f9a17b855c1d595ee8fc3c583bb3f8c2a468
senthilkumar.m <senthilkumar.m@toyotaconnected.co.in>
2023-09-17 12:46:38 +0530

    updated awk command

--- inside_csv/inside_csv.bash
+++ inside_csv/inside_csv.bash
@@ -17,28 +17,22 @@ function display_n_rows_in_a_column()
 	column_name=$2
 	num_rows=$3
 	specific_column_number=$(head -n 1 $file_name | sed 's|,|\n|g' | nl | grep "$column_name" | grep -E "[0-9]+" -o)
-	awk -F',' -v column_number=$specific_column_number '{print $column_number}' $file_name | head -n num_rows
+	awk -F',' -v column_number=$specific_column_number '{print $column_number}' $file_name | head -n $((num_rows + 1))
 }
 
 function filter_a_text_column()
 {
 	file_name=$1
 	column_name=$2
-    text_to_filter=$3
+    text_to_filter="$3"
 	specific_column_number=$(head -n 1 $file_name | sed 's|,|\n|g' | nl | grep "$column_name" | grep -E "[0-9]+" -o)
-	num_of_males=$(awk -F',' -v column_number=$specific_column_number '$column_number=="$text_to_filter" { print }' $file_name | wc -l)
-	echo "Number of males: $num_of_males"
+    echo "The Results are:"
+    echo
+    head -n 1 $file_name
+    echo "---" 
+    awk -F',' -v column_number=$specific_column_number -v text_to_filter_variable="$text_to_filter" '$column_number==text_to_filter_variable { print }' $file_name
 }
 
-function filter_a_numeric_column()
-{
-	file_name=$1
-	column_name=$2
-    numeric_column_condition=$3 # something like ">= 18"
-	specific_column_number=$(head -n 1 $file_name | sed 's|,|\n|g' | nl | grep "$column_name" | grep -E "[0-9]+" -o)
-	age_gt_18=$(awk -F',' -v column_number=$specific_column_number '$column_number $numeric_column_condition { print }' $file_name | wc -l)
-	echo "Num of ppl greater than or equal to 18: $age_gt_18"
-}
 
 if [[ $# -gt 0 ]]; then
     case "$1" in
@@ -50,10 +44,7 @@ if [[ $# -gt 0 ]]; then
             ;;
         --filter_a_text_column)
             filter_a_text_column "$2" "$3" "$4"
-            ;;
-        --filter_a_numeric_column)
-            filter_a_numeric_column "$2" "$3" "$4"
-            ;;            
+            ;;          
         *)
             echo "Unknown option: $1"
             exit 1
